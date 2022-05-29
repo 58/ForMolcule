eaa8dd0dae201fb7e70f52e04005f2766016cab6
Pear <housenka@outlook.com>
2022-05-30 00:08:51 +0900

    Add calculated molcule weight from selected molecule formula

---
+++ formolcule.rb
@@ -0,0 +1,409 @@
+# coding:utf-8
+
+
+def convert_aw(symbol)
+  if symbol == 'H' then
+    return 1.008
+  end
+  if symbol == 'He' then
+    return 4.002
+  end
+  if symbol == 'Li' then
+    return 6.94
+  end
+  if symbol == 'Be' then
+    return 9.012
+  end
+  if symbol == 'B' then
+    return 10.81
+  end
+  if symbol == 'C' then
+    return 12.011
+  end
+  if symbol == 'N' then
+    return 14.007
+  end
+  if symbol == 'O' then
+    return 15.999
+  end
+  if symbol == 'F' then
+    return 18.998
+  end
+  if symbol == 'Ne' then
+    return 20.1797
+  end
+  if symbol == 'Na' then
+    return 22.989
+  end
+  if symbol == 'Mg' then
+    return 24.305
+  end
+  if symbol == 'Al' then
+    return 26.981
+  end
+  if symbol == 'Si' then
+    return 28.085
+  end
+  if symbol == 'P' then
+    return 30.973
+  end
+  if symbol == 'S' then
+    return 32.06
+  end
+  if symbol == 'Cl' then
+    return 35.45
+  end
+  if symbol == 'Ar' then
+    return 39.95
+  end
+  if symbol == 'K' then
+    return 39.0983
+  end
+  if symbol == 'Ca' then
+    return 40.078
+  end
+  if symbol == 'Sc' then
+    return 44.955
+  end
+  if symbol == 'Ti' then
+    return 47.867
+  end
+  if symbol == 'V' then
+    return 50.9415
+  end
+  if symbol == 'Cr' then
+    return 51.9961
+  end
+  if symbol == 'Mn' then
+    return 54.938
+  end
+  if symbol == 'Fe' then
+    return 55.845
+  end
+  if symbol == 'Co' then
+    return 58.933
+  end
+  if symbol == 'Ni' then
+    return 58.6934
+  end
+  if symbol == 'Cu' then
+    return 63.546
+  end
+  if symbol == 'Zn' then
+    return 65.38
+  end
+  if symbol == 'Ga' then
+    return 69.723
+  end
+  if symbol == 'Ge' then
+    return 72.630
+  end
+  if symbol == 'As' then
+    return 74.921
+  end
+  if symbol == 'Se' then
+    return 78.971
+  end
+  if symbol == 'Br' then
+    return 79.904
+  end
+  if symbol == 'Kr' then
+    return 83.798
+  end
+  if symbol == 'Rb' then
+    return 85.4678
+  end
+  if symbol == 'Sr' then
+    return 87.62
+  end
+  if symbol == 'Y' then
+    return 88.905
+  end
+  if symbol == 'Zr' then
+    return 91.224
+  end
+  if symbol == 'Nb' then
+    return 92.906
+  end
+  if symbol == 'Mo' then
+    return 95.95
+  end
+  if symbol == 'Tc' then
+    return 97
+  end
+  if symbol == 'Ru' then
+    return 101.07
+  end
+  if symbol == 'Rh' then
+    return 102.905
+  end
+  if symbol == 'Pd' then
+    return 106.42
+  end
+  if symbol == 'Ag' then
+    return 107.8682
+  end
+  if symbol == 'Cd' then
+    return 112.414
+  end
+  if symbol == 'In' then
+    return 114.818
+  end
+  if symbol == 'Sn' then
+    return 118.710
+  end
+  if symbol == 'Sb' then
+    return 121.760
+  end
+  if symbol == 'Te' then
+    return 127.60
+  end
+  if symbol == 'I' then
+    return 126.904
+  end
+  if symbol == 'Xe' then
+    return 131.293
+  end
+  if symbol == 'Cs' then
+    return 132.905
+  end
+  if symbol == 'Ba' then
+    return 137.327
+  end
+  if symbol == 'La' then
+    return 138.905
+  end
+  if symbol == 'Ce' then
+    return 140.116
+  end
+  if symbol == 'Pr' then
+    return 140.907
+  end
+  if symbol == 'Nd' then
+    return 144.242
+  end
+  if symbol == 'Pm' then
+    return 145
+  end
+  if symbol == 'Sm' then
+    return 150.36
+  end
+  if symbol == 'Eu' then
+    return 151.964
+  end
+  if symbol == 'Gd' then
+    return 157.25
+  end
+  if symbol == 'Tb' then
+    return 158.925
+  end
+  if symbol == 'Dy' then
+    return 162.500
+  end
+  if symbol == 'Ho' then
+    return 164.930
+  end
+  if symbol == 'Er' then
+    return 167.259
+  end
+  if symbol == 'Tm' then
+    return 168.934
+  end
+  if symbol == 'Yb' then
+    return 173.045
+  end
+  if symbol == 'Lu' then
+    return 174.9668
+  end
+  if symbol == 'Hf' then
+    return 178.486
+  end
+  if symbol == 'Ta' then
+    return 180.947
+  end
+  if symbol == 'W' then
+    return 183.84
+  end
+  if symbol == 'Re' then
+    return 186.207
+  end
+  if symbol == 'Os' then
+    return 190.23
+  end
+  if symbol == 'Ir' then
+    return 192.217
+  end
+  if symbol == 'Pt' then
+    return 195.084
+  end
+  if symbol == 'Au' then
+    return 196.966
+  end
+  if symbol == 'Hg' then
+    return 200.592
+  end
+  if symbol == 'Tl' then
+    return 204.38
+  end
+  if symbol == 'Pb' then
+    return 207.2
+  end
+  if symbol == 'Bi' then
+    return 208.980
+  end
+  if symbol == 'Po' then
+    return 209
+  end
+  if symbol == 'At' then
+    return 210
+  end
+  if symbol == 'Rn' then
+    return 222
+  end
+  if symbol == 'Fr' then
+    return 223
+  end
+  if symbol == 'Ra' then
+    return 226
+  end
+  if symbol == 'Ac' then
+    return 227
+  end
+  if symbol == 'Th' then
+    return 232.0377
+  end
+  if symbol == 'Pa' then
+    return 231.035
+  end
+  if symbol == 'U' then
+    return 238.028
+  end
+  if symbol == 'Np' then
+    return 237
+  end
+  if symbol == 'Pu' then
+    return 244
+  end
+  if symbol == 'Am' then
+    return 243
+  end
+  if symbol == 'Cm' then
+    return 247
+  end
+  if symbol == 'Bk' then
+    return 247
+  end
+  if symbol == 'Cf' then
+    return 251
+  end
+  if symbol == 'Es' then
+    return 252
+  end
+  if symbol == 'Fm' then
+    return 257
+  end
+  if symbol == 'Md' then
+    return 258
+  end
+  if symbol == 'No' then
+    return 259
+  end
+  if symbol == 'Lr' then
+    return 262
+  end
+  if symbol == 'Rf' then
+    return 267
+  end
+  if symbol == 'Db' then
+    return 270
+  end
+  if symbol == 'Sg' then
+    return 269
+  end
+  if symbol == 'Bh' then
+    return 270
+  end
+  if symbol == 'Hs' then
+    return 270
+  end
+  if symbol == 'Mt' then
+    return 278
+  end
+  if symbol == 'Ds' then
+    return 281
+  end
+  if symbol == 'Rg' then
+    return 281
+  end
+  if symbol == 'Cn' then
+    return 285
+  end
+  if symbol == 'Nh' then
+    return 286
+  end
+  if symbol == 'Fl' then
+    return 289
+  end
+  if symbol == 'Mc' then
+    return 289
+  end
+  if symbol == 'Lv' then
+    return 293
+  end
+  if symbol == 'Ts' then
+    return 293
+  end
+  if symbol == 'Og' then
+    return 294
+  end
+end
+
+text = 'CuSO4(H2O)5'
+
+# no bracket or bracket
+mf_units = text.scan(/\(?\w+\)?\d*/)
+
+mf_h = {}
+mf_units.each do |mf_unit|
+  if mf_unit.include? '(' then
+    # delete brackets & split number of molecule
+    mfs = mf_unit.delete('(').split(')')
+    atom_units = mfs[0].scan(/[A-z][a-z]?\d?/)
+
+    atom_units.each do |i|
+      element = i.delete("^a-zA-z")
+      num_atom = i.delete("^0-9").to_i == 0 ? 1 : i.delete("^0-9").to_i
+
+      if mf_h.key?(element) then
+        mf_h[element] += 1 * mfs[1].to_i
+      else
+        mf_h.store(element, num_atom * mfs[1].to_i)
+      end
+    end
+  else
+    # {element}{number} style
+    atom_units = mf_unit.scan(/[A-z][a-z]?\d?/)
+    atom_units.each do |atom_unit|
+      element = atom_unit.delete("^a-zA-z")
+
+      # number of element ommision treatment
+      num_atom = atom_unit.delete("^0-9").to_i == 0 ? 1 : atom_unit.delete("^0-9").to_i
+
+      if mf_h.key?(element) then
+        mf_h[element] += 1
+      else
+        mf_h.store(element, num_atom)
+      end
+    end
+  end
+end
+
+chemical_formula = ''
+molecule_weight = 0
+mf_h.each do |s, num|
+  chemical_formula << "#{s}#{num}"
+  molecule_weight += convert_aw(s) * num
+end
+p text
+p chemical_formula
+p molecule_weight
